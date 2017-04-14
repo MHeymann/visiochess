bb6c937b09c11501e857020b505052de7b66f6d1
Elan van Biljon <elanvanbiljon@gmail.com>
2017-04-14 15:19:40 +0200

    Change servername to ip
    Change-Id: I095ff3e43a9742ba8a2ceb4b20ea8eb9b2bdd594

--- php/create_db.php
+++ php/create_db.php
@@ -4,10 +4,10 @@ function create_database($db_name, $target_file) {
 	/* just for testing */
 	$max_move_length = get_longest_moves_string($target_file);
 	echo "The length of the longest move is: ". $max_move_length;
-	
+
 	$settings = parse_ini_file(__DIR__."/../.my.cnf", true);
 
-	$servername = "localhost";
+	$servername = "127.0.0.1"; // needs ip on osx, will this work on Ubuntu?
 	$username = $settings['client']['user'];
 	$password = $settings['client']['password'];
 	/*
