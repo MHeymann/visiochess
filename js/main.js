3905097ce452148f57ca0204315a3aab69bb8df7
Elan van Biljon <elanvanbiljon@gmail.com>
2017-04-17 07:43:48 +0200

    Add basic form and ajax to send form data
    The html is just to have something to test the ajax with, the design of
    that has been done already, I believe and for now I will leave the
    asthetic to someone else. The javascript is also very basic but we don't
    need anything too fancy here.
    
    Change-Id: I941a85388c802d4670923938cdcca9a963fb9da2

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ function handle_onsubmit(e) {
 
 		reader.readAsText(file);
 
-
 	} else {
 		console.log("Something is wrong with the file input object...");
 		if ('files' in fileSubmitter && (fileSubmitter.files.length <= 0)) {
@@ -31,7 +30,50 @@ function handle_onsubmit(e) {
 	console.log("clicked submit\n");
 }
 
+function getFormData($form){
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i){
+        indexed_array[n['name']] = n['value'];
+    });
+
+    return indexed_array;
+}
+
+function handle_filter_submit(event) {
+	event.preventDefault();
+
+	var $form = $('#filter_form');
+	send_url = $form.attr("action");
+	console.log("action url: ", send_url);
+	var filters = getFormData($form);
+
+	console.log(
+		'Sending these filters:\n',
+		filters
+	);
+
+	alert('Sending filters, look at console');
+
+	$.ajax({
+		// this needs to be changed such that it fetces this from the config file
+		url: send_url,
+		type: 'post',
+		data: filters,
+		dataType: 'html',
+		success: function(response) {
+			console.log(response);
+			// render results
+		},
+		error: function (xhr, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+	});
+}
+
 window.onload = function() {
-	document.getElementById("up_form").onsubmit = handle_onsubmit;
+	$("#up_form").submit(handle_onsubmit);
+	$('#filter_form').submit(handle_filter_submit);
 	console.log("Hello!");
 }
