cc450b6972d58e92ced76fd55d23b9b73a3911d5
Elan van Biljon <elanvanbiljon@gmail.com>
2017-04-24 23:18:37 +0200

    Add new functionality to MySQL interface
    The MySQL interface has been extended to be able to insert data into a
    database and to perform "select from" type queries.
    
    Change-Id: I2dbdd9953837fe35a695c26cb84a59597334aeec

--- php/query.php
+++ php/query.php
@@ -12,27 +12,4 @@ foreach($_POST as $filter_field => $value) {
 echo "<p>";
 echo json_encode($filters);
 echo "</p>";
-// only consists of test code demoing how the interface object works
-
-$settings = parse_ini_file(__DIR__."/../.my.cnf", true);
-$servername = $settings['client']['mysql_server'];
-$username = $settings['client']['user'];
-$password = $settings['client']['password'];
-
-$db = new MySqlPhpInterface($server=$servername, $user=$username, $password=$password);
-$db->connect();
-$db->create_database('test');
-$db->create_database($name='test', $replace=true);
-$db->use_database('test');
-$db->create_table(
-  'test',
-  array(
-    'test_col' => 'varchar(15)',
-    'test_col2' => 'int'
-  ));
-$db->delete_table('test');
-$db->delete_database('test');
-$db->sql('create database test;');
-$db->sql('drop database test;');
-$db->disconnect();
 ?>
