c7adfba48077e9488c30f5a3dc551fb066893ce2
Murray Heymann <heymann.murray@gmail.com>
2017-05-04 14:47:31 +0200

    Upgrade php/pgn_parser.php: whitespace and comments
    Until now, we assumed the first seven tags, as well as the optional tags
    wouldn't have whitespaces in between.  This updated now allows whitespace
    to be present in any amount between lines and also validate the correctness
    of tags before scanning them.
    
    Many syntax errors in the millionbase file are unmasked now:
    There are many (MANY) games in which no moves are indicated, only the score
    where the moves should have been.  Quite a number of games have a deleted
    first move line and just abrubly start move annotation on the sixth move.
    
    While this is not critical for our implementation, any "additional" work we
    might still consider might be influenced by this.
    
    Change-Id: I107a535b079890b78791ce78383b6a4a9e5cd737

--- php/create_default_db.php
+++ php/create_default_db.php
@@ -3,5 +3,5 @@ require_once "create_db.php";
 
 create_database("default_chess_db","/data/millionbase-2.5.pgn");
 parse_pgn_file_to_db("/data/millionbase-2.5.pgn", "default_chess_db", 200,
-	$verbose=false);
+	$verbose=true);
 ?>
