f443a22ff0f1795701a57da32cc3feb7e5551389
Elan van Biljon <elanvanbiljon@gmail.com>
2017-05-04 17:07:35 +0200

    Handle single or no data point queries
    When the user filters the data there is always the chance that they
    specify the filters in such a way that the database only has 1 entry (or
    none) that satisfies them. If there is only one entry, the visualisation
    needs to be adapted (this happends in visual_response.js), if there is
    no data returned, the server returns an error (this is in query.php).
    
    Change-Id: I3742fa761d0820623357ccb790e995cf82f09368

--- js/visual_response.js
+++ js/visual_response.js
@@ -12,115 +12,112 @@ var currentData = null;
  * @param json_data A JSON object of the data generated in php/query.php
  */
 function draw(json_data) {
-  /* area in html for graph */
+	/* area in html for graph */
+	/* the categories */
+	var keys = json_data.pop;
+
+	/* the data */
+	var data = process_JSON_to_D3(json_data, keys);
+
+	var svg = d3.select("#display_svg").append("svg")
+	.style("width", 960)
+	.style("height", ($("#display_svg").width() * 0.666))
+	.attr("viewBox", "0 0 960 " + ($("#display_svg").width() * 0.666))
+	.attr("preserveAspectRatio", "xMinYMin meet"),
+	margin = {
+		top: 20,
+		right: 20,
+		bottom: 30,
+		left: 50
+	};
+
+	var disp_w = $("#display_svg").width();
+	var disp_h = $("#display_svg").height();
+
+	var width = 0;
+	var height = 0;
+	if (disp_h * 1.5 < disp_w) {
+		width = disp_h * 1.5 - margin.left - margin.right;
+		height = disp_h - margin.top - margin.bottom;
+	} else {
+		width = disp_w - margin.left - margin.right;
+		height = disp_w * 0.66667 - margin.top - margin.bottom;
+	}
 
 
-  var svg = d3.select("#display_svg").append("svg")
-    .style("width", 960)
-    .style("height", ($("#display_svg").width() * 0.666))
-    .attr("viewBox", "0 0 960 " + ($("#display_svg").width() * 0.666))
-    .attr("preserveAspectRatio", "xMinYMin meet"),
-    margin = {
-      top: 20,
-      right: 20,
-      bottom: 30,
-      left: 50
-    };
+	/* the years for x-axis*/
+	var x = d3.scaleTime().range([0, width]),
+	/* popularity for y-axis */
+	y = d3.scaleLinear().range([height, 0]),
+	/* colours for each category */
+	z = d3.scaleOrdinal(d3.schemeCategory10);
 
-  var disp_w = $("#display_svg").width();
-  var disp_h = $("#display_svg").height();
+	var stack = d3.stack();
 
-  var width = 0;
-  var height = 0;
-  if (disp_h * 1.5 < disp_w) {
-    width = disp_h * 1.5 - margin.left - margin.right;
-    height = disp_h - margin.top - margin.bottom;
-  }
-  else {
-    width = disp_w - margin.left - margin.right;
-    height = disp_w * 0.66667 - margin.top - margin.bottom;
-  }
+	/* creates the area on the graph for each category */
+	var area = d3.area()
+	.x(function(d, i) { // x-coordinates of the shape
+		return x(parseDate(d.data.year));
+	})
+	.y0(function(d, i) { // bottom y-coordinates of the shape
+		return y(d[0]);
+	})
+	.y1(function(d) { //top y-coordinates of the shape
+		return y(d[1]);
+	});
 
 
-  /* the years for x-axis*/
-  var x = d3.scaleTime().range([0, width]),
-    /* popularity for y-axis */
-    y = d3.scaleLinear().range([height, 0]),
-    /* colours for each category */
-    z = d3.scaleOrdinal(d3.schemeCategory10);
+	/* group in html to place resulting graph in */
+	var g = svg.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var stack = d3.stack();
+	/* the years */
+	x.domain(d3.extent(data, function(d, i) {
+		return parseDate(d.year);
+	}));
 
-  /* creates the area on the graph for each category */
-  var area = d3.area()
-    .x(function(d, i) { // x-coordinates of the shape
-      return x(parseDate(d.data.year));
-    })
-    .y0(function(d, i) { // bottom y-coordinates of the shape
-      return y(d[0]);
-    })
-    .y1(function(d) { //top y-coordinates of the shape
-      return y(d[1]);
-    });
+	/* colours */
+	z.domain(keys);
 
+	/* creates a stack consisting of the different categories */
+	stack.keys(keys);
 
-/* group in html to place resulting graph in */
-  var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	/* creates layers consisting of each area graph for the different categories */
+	var layer = g.selectAll(".layer")
+	.data(stack(data))
+	.enter().append("g")
+	.attr("class", "layer");
 
-  /* the categories */
-  var keys = json_data.pop;
+	layer.append("path")
+	.attr("class", "area")
+	.style("fill", function(d, i) {
+		return z(keys[i]);
+	})
+	.attr("d", area);
 
-  /* the data */
-  var data = process_JSON_to_D3(json_data, keys);
+	layer.filter(function(d) {
+		return d[d.length - 1][1] - d[d.length - 1][0] > 0.01;
+	})
+	.append("text")
+	.attr("x", width - 6)
+	.attr("y", function(d) {
+		return y((d[d.length - 1][0] + d[d.length - 1][1]) / 2);
+	})
+	.attr("dy", ".35em")
+	.style("font", "10px sans-serif")
+	.style("text-anchor", "end")
+	.text(function(d) {
+		return d.key;
+	});
 
-  /* the years */
-  x.domain(d3.extent(data, function(d, i) {
-    return parseDate(d.year);
-  }));
+	g.append("g")
+	.attr("class", "axis axis--x")
+	.attr("transform", "translate(0," + height + ")")
+	.call(d3.axisBottom(x));
 
-  /* colours */
-  z.domain(keys);
-
-  /* creates a stack consisting of the different categories */
-  stack.keys(keys);
-
-  /* creates layers consisting of each area graph for the different categories */
-  var layer = g.selectAll(".layer")
-    .data(stack(data))
-    .enter().append("g")
-    .attr("class", "layer");
-
-  layer.append("path")
-    .attr("class", "area")
-    .style("fill", function(d, i) {
-      return z(keys[i]);
-    })
-    .attr("d", area);
-
-  layer.filter(function(d) {
-      return d[d.length - 1][1] - d[d.length - 1][0] > 0.01;
-    })
-    .append("text")
-    .attr("x", width - 6)
-    .attr("y", function(d) {
-      return y((d[d.length - 1][0] + d[d.length - 1][1]) / 2);
-    })
-    .attr("dy", ".35em")
-    .style("font", "10px sans-serif")
-    .style("text-anchor", "end")
-    .text(function(d) {
-      return d.key;
-    });
-
-  g.append("g")
-    .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
-  g.append("g")
-    .attr("class", "axis axis--y")
-    .call(d3.axisLeft(y).ticks(10, "%"));
+	g.append("g")
+	.attr("class", "axis axis--y")
+	.call(d3.axisLeft(y).ticks(10, "%"));
 }
 
 /**
@@ -131,25 +128,34 @@ function draw(json_data) {
  * @return data The generated JSON object
  */
 function process_JSON_to_D3(json_data, keys) {
-  var data = [];
+	var data = [];
 
-  if (hex_sha256(JSON.stringify(json_data)) == currentJSONHash) {
-    data = currentData;
-  }
-  else {
-    /* 'data' from json (would be better if array received from php)*/
+	if (hex_sha256(JSON.stringify(json_data)) == currentJSONHash) {
+		data = currentData;
+	} else {
+		/* 'data' from json (would be better if array received from php)*/
 
-    Object.keys(json_data.data).forEach(function(key, i) {
-      var entry = {};
-      entry['year'] = parseInt(key);
-      json_data.data[key].forEach(function(v, i) {
-        entry[keys[i]] = v;
-      });
-      data.push(entry);
-    });
-    currentData = data;
-    currentJSONHash = hex_sha256(JSON.stringify(json_data));
-  }
+		for (var year in json_data.data) {
+			var entry = {};
+			entry['year'] = parseInt(year);
+			for (var index in json_data.data[year]) {
+				entry[keys[index]] = json_data.data[year][index];
+			}
+			data.push(entry);
+		}
 
-  return data;
+		if(data.length == 1) {
+			var entry = {};
+			for (key in data[0]) {
+				entry[key] = data[0][key];
+			}
+			entry['year']++;
+			$.extend(data, {1: entry});
+		}
+
+		currentData = data;
+		currentJSONHash = hex_sha256(JSON.stringify(json_data));
+	}
+
+	return data;
 }
